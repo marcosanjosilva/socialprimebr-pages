26f0de6f58f76a0a1756fb8f5355bab7266be60b
marcosanjosilva <marcosanjos.lk@gmail.com>
2025-08-28 00:48:34 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+<script>
+  // Troque por suas credenciais do novo projeto
+  window.SPB = {
+    url: 'https://ojxabliahndxbgnaitqc.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qeGFibGlhaG5keGJnbmFpdHFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMzUzOTUsImV4cCI6MjA3MTkxMTM5NX0.KuVPdhzJrBDvgK1JM0LXIKzB-l7ufh3cterzy3MaZbE'
+  };
+</script>
